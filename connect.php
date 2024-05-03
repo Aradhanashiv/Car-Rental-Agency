f020af28ec2678d78c94d64289d9b1a279b3350d
Aradhana <140301485+Aradhanashiv@users.noreply.github.com>
2024-05-03 13:38:17 +0530

    Add files via upload

---
+++ connect.php
@@ -0,0 +1,12 @@
+<?php
+$conn = mysqli_connect("localhost" , "root", "" , "car_agency");
+if(!$conn){
+    die ("connection unsuccessful");
+}
+
+// $conn = mysqli_connect("sql109.infinityfree.com" , "if0_36473034", "lQOCxOZi16a4QFr" , "if0_36473034_mysqloperations");
+// if(!$conn){
+//     die ("connection unsuccessful");
+// }
+
+?>
